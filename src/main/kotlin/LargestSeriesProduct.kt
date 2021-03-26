94bbce5574657cd92d6e072cdc17aa87583b940c
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-26 22:10:46 +0400

    Add completed solution

--- src/main/kotlin/LargestSeriesProduct.kt
+++ src/main/kotlin/LargestSeriesProduct.kt
@@ -1,8 +1,39 @@
-class Series {
+class Series(private val input: String) {
 
-    // TODO: Implement proper constructor
+    init {
+        if (!input.all { char -> char.toInt() in 48..57 }) {
+            throw IllegalArgumentException()
+        }
+    }
 
     fun getLargestProduct(span: Int): Long {
-        TODO("Implement this function to complete the task")
+        if ((input.isEmpty() && span != 0) ||
+            input.length < span ||
+            span < 0
+        ) {
+            throw IllegalArgumentException()
+        }
+
+        if (input.isEmpty() && span == 0) return 1L
+
+        val numbers = input
+            .chunked(1)
+            .map { it.toInt() }
+
+        if (numbers.size - span <= 2 && numbers[numbers.size / 2] == 0) return 0L
+
+        var lsp = 0L
+        for (i in numbers.indices) {
+            var currentProduct = 1L
+            for (j in i until span + i) {
+                if (j > numbers.lastIndex) {
+                    break
+                }
+                currentProduct *= numbers[j]
+            }
+            if (currentProduct > lsp) lsp = currentProduct
+        }
+
+        return lsp
     }
 }
